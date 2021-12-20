6dcc60aaedb0e191a2b6e26efa9663b347137fa2
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-20 15:52:05 +0900

    WeatherAPi Services 2

--- lib/services/weather_api_services.dart
+++ lib/services/weather_api_services.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:weather_provider/constants/constants.dart';
 import 'package:weather_provider/exceptions/weather_exception.dart';
+import 'package:weather_provider/models/weather.dart';
 import 'package:weather_provider/services/http_error_handler.dart';
 
 class WeatherApiservices {
@@ -24,7 +25,7 @@ class WeatherApiservices {
       final http.Response response = await http.get(uri);
 
       if (response.statusCode != 200) {
-        throw httpErrorHandler(response);
+        throw Exception(httpErrorHandler(response));
       }
 
       final responseBody = json.decode(response.body);
@@ -34,10 +35,35 @@ class WeatherApiservices {
       }
 
       if (responseBody.length > 1) {
-        throw WeatherException('There are multiple candidates for city, please specify furthur!');
+        throw WeatherException(
+            'There are multiple candidates for city, please specify furthur!');
       }
 
       return responseBody[0]['woeid'];
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Weather> getWeather(int woeid) async {
+    final Uri uri = Uri(
+      scheme: 'https',
+      host: kHost,
+      path: '/api/location/$woeid',
+    );
+
+    try {
+      final http.Response response = await http.get(uri);
+
+      if (response.statusCode != 200) {
+        throw Exception(httpErrorHandler(response));
+      }
+
+      final weatherJson = json.decode(response.body);
+      final Weather weather = Weather.fromJson(weatherJson);
+      print(weather);
+
+      return weather;
     } catch (e) {
       rethrow;
     }
